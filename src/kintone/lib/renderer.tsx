49ae3659768780d711b21645076fc73ec6b49c28
SHIDA Yuma <yuma_shida@shidax.co.jp>
2025-09-16 18:57:28 +0900

    fix(kintone): 一覧画面のページャーが動かなかった問題を修正した
    Fixes #48

---
+++ src/kintone/lib/renderer.tsx
@@ -0,0 +1,32 @@
+import React, { type ReactElement } from 'react';
+import { type Root, createRoot } from 'react-dom/client';
+
+import GlobalStyle, { type GlobalStyleOptions } from '@components/GlobalStyle';
+
+export default class Renderer {
+  private root: Root | null = null;
+  private container: HTMLElement | null = null;
+
+  render(
+    container: HTMLElement,
+    element: ReactElement,
+    opts?: GlobalStyleOptions
+  ): void {
+    if (this.container !== container) {
+      this.unmount();
+    }
+    if (!this.root) {
+      this.container = container;
+      this.root = createRoot(container);
+    }
+
+    this.root.render(<GlobalStyle {...opts}>{element}</GlobalStyle>);
+  }
+
+  unmount(): void {
+    if (this.root) {
+      this.root.unmount();
+      this.root = null;
+    }
+  }
+}
