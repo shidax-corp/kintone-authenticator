fd04cba93df9ff1c8262cac9bc5a5742bcfa2f9a
SHIDA Yuma <yuma_shida@shidax.co.jp>
2025-10-17 13:26:33 +0900

    style: 無用だったprettierignoreを削除した

--- src/kintone/kintone.d.ts
+++ src/kintone/kintone.d.ts
@@ -1,6 +1,8 @@
 declare namespace kintone {
   namespace events {
-    type RecordIndexShowEventType = 'app.record.index.show' | 'mobile.app.record.index.show';
+    type RecordIndexShowEventType =
+      | 'app.record.index.show'
+      | 'mobile.app.record.index.show';
     interface RecordIndexShowEvent {
       type: RecordIndexShowEventType;
       appId: number;
@@ -8,7 +10,9 @@ declare namespace kintone {
       records: kintone.types.SavedFields[];
     }
 
-    type RecordDetailShowEventType = 'app.record.detail.show' | 'mobile.app.record.detail.show';
+    type RecordDetailShowEventType =
+      | 'app.record.detail.show'
+      | 'mobile.app.record.detail.show';
     interface RecordDetailShowEvent {
       type: RecordDetailShowEventType;
       appId: number;
@@ -16,14 +20,18 @@ declare namespace kintone {
       record: kintone.types.SavedFields;
     }
 
-    type RecordCreateShowEventType = 'app.record.create.show' | 'mobile.app.record.create.show';
+    type RecordCreateShowEventType =
+      | 'app.record.create.show'
+      | 'mobile.app.record.create.show';
     interface RecordCreateShowEvent {
       type: RecordCreateShowEventType;
       appId: number;
       record: kintone.types.Fields;
     }
 
-    type RecordEditShowEventType = 'app.record.edit.show' | 'mobile.app.record.edit.show';
+    type RecordEditShowEventType =
+      | 'app.record.edit.show'
+      | 'mobile.app.record.edit.show';
     interface RecordEditShowEvent {
       type: RecordEditShowEventType;
       appId: number;
@@ -31,14 +39,18 @@ declare namespace kintone {
       record: kintone.types.SavedFields;
     }
 
-    type RecordCreateSubmitEventType = 'app.record.create.submit' | 'mobile.app.record.create.submit';
+    type RecordCreateSubmitEventType =
+      | 'app.record.create.submit'
+      | 'mobile.app.record.create.submit';
     interface RecordCreateSubmitEvent {
       type: RecordCreateSubmitEventType;
       appId: number;
       record: kintone.types.Fields;
     }
 
-    type RecordEditSubmitEventType = 'app.record.edit.submit' | 'mobile.app.record.edit.submit';
+    type RecordEditSubmitEventType =
+      | 'app.record.edit.submit'
+      | 'mobile.app.record.edit.submit';
     interface RecordEditSubmitEvent {
       type: RecordEditSubmitEventType;
       appId: number;
@@ -46,12 +58,30 @@ declare namespace kintone {
       record: kintone.types.SavedFields;
     }
 
-    function on(eventTypes: RecordIndexShowEventType | RecordIndexShowEventType[], handler: (event: RecordIndexShowEvent) => RecordIndexShowEvent): void;
-    function on(eventTypes: RecordDetailShowEventType | RecordDetailShowEventType[], handler: (event: RecordDetailShowEvent) => RecordDetailShowEvent): void;
-    function on(eventTypes: RecordCreateShowEventType | RecordCreateShowEventType[], handler: (event: RecordCreateShowEvent) => RecordCreateShowEvent): void;
-    function on(eventTypes: RecordEditShowEventType | RecordEditShowEventType[], handler: (event: RecordEditShowEvent) => RecordEditShowEvent): void;
-    function on(eventTypes: RecordCreateSubmitEventType | RecordCreateSubmitEventType[], handler: (event: RecordCreateSubmitEvent) => RecordCreateSubmitEvent): void;
-    function on(eventTypes: RecordEditSubmitEventType | RecordEditSubmitEventType[], handler: (event: RecordEditSubmitEvent) => RecordEditSubmitEvent): void;
+    function on(
+      eventTypes: RecordIndexShowEventType | RecordIndexShowEventType[],
+      handler: (event: RecordIndexShowEvent) => RecordIndexShowEvent
+    ): void;
+    function on(
+      eventTypes: RecordDetailShowEventType | RecordDetailShowEventType[],
+      handler: (event: RecordDetailShowEvent) => RecordDetailShowEvent
+    ): void;
+    function on(
+      eventTypes: RecordCreateShowEventType | RecordCreateShowEventType[],
+      handler: (event: RecordCreateShowEvent) => RecordCreateShowEvent
+    ): void;
+    function on(
+      eventTypes: RecordEditShowEventType | RecordEditShowEventType[],
+      handler: (event: RecordEditShowEvent) => RecordEditShowEvent
+    ): void;
+    function on(
+      eventTypes: RecordCreateSubmitEventType | RecordCreateSubmitEventType[],
+      handler: (event: RecordCreateSubmitEvent) => RecordCreateSubmitEvent
+    ): void;
+    function on(
+      eventTypes: RecordEditSubmitEventType | RecordEditSubmitEventType[],
+      handler: (event: RecordEditSubmitEvent) => RecordEditSubmitEvent
+    ): void;
   }
 
   namespace app {
@@ -64,7 +94,9 @@ declare namespace kintone {
       function getFieldElement(fieldCode: string): HTMLDivElement | null;
       function getSpaceElement(spaceId: string): HTMLDivElement | null;
       function setFieldShown(fieldCode: string, isShown: boolean): void;
-      function get(): { record: kintone.types.Fields | kintone.types.SavedFields };
+      function get(): {
+        record: kintone.types.Fields | kintone.types.SavedFields;
+      };
       function set(params: { record: kintone.types.Fields }): Promise<void>;
     }
   }
@@ -77,14 +109,37 @@ declare namespace kintone {
       namespace record {
         function getSpaceElement(spaceId: string): HTMLDivElement | null;
         function setFieldShown(fieldCode: string, isShown: boolean): void;
-        function get(): { record: kintone.types.Fields | kintone.types.SavedFields };
+        function get(): {
+          record: kintone.types.Fields | kintone.types.SavedFields;
+        };
         function set(params: { record: kintone.types.Fields }): Promise<void>;
       }
     }
   }
 
-  function api(pathOrUrl: '/k/v1/records/cursor.json', method: 'POST', params: { app: number | string, fields?: string[], query?: string, size?: number | string }): Promise<{ id: string, totalCount: number }>;
-  function api(pathOrUrl: '/k/v1/records/cursor.json', method: 'GET', params: { id: string }): Promise<{ records: kintone.types.SavedFields[], next: boolean }>;
-  function api(pathOrUrl: '/k/v1/records/cursor.json', method: 'DELETE', params: { id: string }): Promise<{ [key: string]: never }>;
-  function api(pathOrUrl: string, method: string, params: Record<string, any>): Promise<Record<string, any>>;
+  function api(
+    pathOrUrl: '/k/v1/records/cursor.json',
+    method: 'POST',
+    params: {
+      app: number | string;
+      fields?: string[];
+      query?: string;
+      size?: number | string;
+    }
+  ): Promise<{ id: string; totalCount: number }>;
+  function api(
+    pathOrUrl: '/k/v1/records/cursor.json',
+    method: 'GET',
+    params: { id: string }
+  ): Promise<{ records: kintone.types.SavedFields[]; next: boolean }>;
+  function api(
+    pathOrUrl: '/k/v1/records/cursor.json',
+    method: 'DELETE',
+    params: { id: string }
+  ): Promise<{ [key: string]: never }>;
+  function api(
+    pathOrUrl: string,
+    method: string,
+    params: Record<string, any>
+  ): Promise<Record<string, any>>;
 }
